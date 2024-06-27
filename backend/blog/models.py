86890887a93b41c6dcf3110e02c4315ca4f09c05
AgustinAdmetlla <agustin_facundo@yahoo.com>
2024-06-26 22:02:51 -0300

    finCreacionAppBlog

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,10 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 def user_directory_path(instance, filename):
     return 'blog/{0}/{1}'.format(instance.title, filename)
 
 class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_wueryset(self):
+            return super().get_queryset().filter(status='published')
+        
+        
+    class Meta:
+        ordering = ('-published', )
+
+    
+
+    options = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+
     title = models.CharField(max_length=250)
     thumbnail = models.ImageField(
         upload_to = user_directory_path, 
@@ -14,9 +32,27 @@ class Post(models.Model):
     )
     excerpt = models.TextField(null=True)
     content = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
     # slug es para agregar contenido
-    slug = models.SlugField(max_length=250, unique_for_date='published')
+    slug = models.SlugField(
+            max_length=250, 
+            unique_for_date='published',
+            null = False,
+            unique = True
+    )
+    author = models.ForeignKey(
+        User, 
+        on_delete = models.CASCADE,
+        related_name = 'post_user'
+    )
 
+    status = models.CharField(
+        max_length = 10, 
+        choices = options, 
+        default = 'draft'
+    )
+    objects = models.Manager()
+    postobjects = PostObjects()
 
 
 
