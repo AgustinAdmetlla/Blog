1b090f95b9c98456fab85b6b18adc910913e8cf0
AgustinAdmetlla <agustin_facundo@yahoo.com>
2024-06-27 01:45:41 -0300

    desarrolloVUE

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -5,5 +5,5 @@ from .views import PostDetailView
 app_name = 'blog'
 urlpatterns = [
     path('posts/', BlogListView.as_view()),
-    path('posts/<post_slug>', PostDetailView.as_view())
+    path('posts/<post_slug>/', PostDetailView.as_view())
 ]
