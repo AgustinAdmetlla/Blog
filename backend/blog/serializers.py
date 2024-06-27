835c5c62277c5ce1a75babede74d781a16ce9737
AgustinAdmetlla <agustin_facundo@yahoo.com>
2024-06-27 00:17:52 -0300

    inicioVistas

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Post
 
-class PostSerializer(serializer.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = (
